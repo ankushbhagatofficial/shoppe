f9dee91da3c83a0a343a6fa55d5894c97d6f5e98
Ankush Bhagat <ankushbhagatofficial@gmail.com>
2026-05-30 21:22:36 +0530

    feat: proxy, auth detection

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,20 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
+import { auth } from "./lib/auth";
 
-export function proxy(request: NextRequest) {
+export async function proxy(request: NextRequest) {
+  const { pathname } = request.nextUrl
+  const isLogged = await auth()
+  if (isLogged && ["/login", "/register"].some((path) => pathname.startsWith(path))) {
+    return NextResponse.redirect(new URL("/", request.url))
+  }
+
+  return NextResponse.next()
 
 }
+
+export const config = {
+  matcher: ["/login/:path*", "/register/:path*"]
+}
+
 
